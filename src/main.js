691c08b57e595ed5778033372d046cb3c50f1327
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-03-21 00:38:15 +0200

    start2

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import renderFunction from './js/render-function';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+console.log(makeMessage('Jacob')); // "Welcom, Jacob!"
